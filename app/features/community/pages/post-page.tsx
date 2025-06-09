f3dccab81b22f17481111b02395953cdd0d559a8
Juwon Lee <a8316010@gmail.com>
2025-06-09 16:17:00 +0900

    Create Post Page

--- app/features/community/pages/post-page.tsx
+++ app/features/community/pages/post-page.tsx
@@ -1,4 +1,20 @@
 import type { Route } from "common/pages/+types";
+import { ChevronUpIcon, DotIcon, SlashIcon } from "lucide-react";
+import { Form, Link } from "react-router";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "~/common/components/ui/avatar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "~/common/components/ui/breadcrumb";
+import { Button } from "~/common/components/ui/button";
+import { Textarea } from "~/common/components/ui/textarea";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -8,5 +24,75 @@ export const meta: Route.MetaFunction = () => {
 };
 
 export default function PostPage() {
-  return <div className="container py-8"></div>;
+  return (
+    <div className="grid grid-cols-6 gap-40 items-start">
+      <div className="col-span-4 space-y-10">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link to="/community">Community</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link to="/community?topic=productivity">Productivity</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link to="/community/postId">
+                  What is the best productivity tool?
+                </Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="flex w-full items-start gap-10">
+          <Button variant="outline" className="flex flex-col h-14">
+            <ChevronUpIcon className="size-4 shrink-0" />
+            <span>10</span>
+          </Button>
+          <div className="space-y-20">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold">
+                What is the best productivity tool?
+              </h2>
+              <div className="flex items-center gap-2 text-sm text-muted-foreground">
+                <span>@nico</span>
+                <DotIcon className="size-5" />
+                <span>12 hours ago</span>
+                <DotIcon className="size-5" />
+                <span>10 replies</span>
+              </div>
+              <p className="text-muted-foreground w-3/4">
+                Hello, I'm looking for a productivity tool that can help me
+                manage my tasks and projects. Any recommendations? I have tried
+                Notion, but it's not what I'm looking for. I dream of a tool
+                that can help me manage my tasks and projects. Any
+                recommendations?
+              </p>
+            </div>
+            <Form className="flex items-start gap-5 w-3/4">
+              <Avatar className="size-14">
+                <AvatarFallback>N</AvatarFallback>
+                <AvatarImage src="https://github.com/jw1011.png" />
+              </Avatar>
+              <div className="flex flex-col gap-5 items-end w-full">
+                <Textarea
+                  placeholder="Write a reply"
+                  className="w-full resize-none"
+                  rows={5}
+                />
+                <Button>Reply</Button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
+      <aside className="col-span-2"></aside>
+    </div>
+  );
 }
--- app/features/community/pages/submit-post-page.tsx
+++ app/features/community/pages/submit-post-page.tsx
@@ -1,12 +1,57 @@
 import type { Route } from "common/pages/+types";
 
+import { Form } from "react-router";
+import { Hero } from "~/common/components/hero";
+import InputPair from "~/common/components/input-pair";
+import SelectPair from "~/common/components/select-pair";
+import { Button } from "~/common/components/ui/button";
+
 export const meta: Route.MetaFunction = () => {
   return [
-    { title: "Join | wemake" },
+    { title: "Submit | wemake" },
     { name: "description", content: "회원가입 페이지" },
   ];
 };
 
 export default function SubmitPostPage() {
-  return <div className="container py-8"></div>;
+  return (
+    <div>
+      <Hero
+        title="Create Discussion"
+        subtitle="Ask questions, share ideas, and connect with other developers"
+      ></Hero>
+      <Form className="flex flex-col gap-10 max-w-screen-md mx-auto">
+        <InputPair
+          label="Title"
+          name="title"
+          id="title"
+          description="(40 characters of less)"
+          required
+          placeholder="i.e what is the best productivity tool?"
+        ></InputPair>
+        <SelectPair
+          required
+          name="category"
+          label="Category"
+          description="Select the category that best fits your discussion"
+          placeholder="i.e Productivity"
+          options={[
+            { label: "Productivity", value: "productivity" },
+            { label: "Programming", value: "programming" },
+            { label: "Design", value: "design" },
+          ]}
+        ></SelectPair>
+        <InputPair
+          label="Content"
+          name="content"
+          id="content"
+          description="(1000 characters of less)"
+          required
+          placeholder="i.e I'm looking for a tool that can help me manage my time and tasks. What are the best tools out there?"
+          textArea
+        ></InputPair>
+        <Button className="mx-auto">Create Discussion</Button>
+      </Form>
+    </div>
+  );
 }
